ca92b99c715bba166fc84206c6988df88b2387d0
th3-k3rnl <aaron.ilift@gmail.com>
2022-10-30 20:21:04 +0000

    Adding Datetime stamp to Employee Entity class

--- demo/src/main/java/com/oracle/model/Employee.java
+++ demo/src/main/java/com/oracle/model/Employee.java
@@ -1,5 +1,7 @@
 package com.oracle.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +14,12 @@ import javax.persistence.Table;
 public class Employee {
 
     private long id;
+
+    // The date and time when this Entity class was instantiated
+    // which should be around about the time when the post request
+    // is submitted to the REST API
+    private Date submitted = new Date();
+
     private String firstName;
     private String lastName;
     private String emailId;
@@ -36,7 +44,16 @@ public class Employee {
         this.id = id;
     }
 
-    @Column(name = "first_name", nullable = false)
+    @Column(name = "submitted", nullable = false)
+    public Date getSubmitted() {
+        return submitted;
+    }
+
+    public void setSubmitted(Date submitted) {
+        this.submitted = submitted;
+    }
+
+    @Column(name = "first_name", nullable = false, length = 64)
     public String getFirstName() {
         return firstName;
     }
@@ -45,7 +62,7 @@ public class Employee {
         this.firstName = firstName;
     }
 
-    @Column(name = "last_name", nullable = false)
+    @Column(name = "last_name", nullable = false, length = 64)
     public String getLastName() {
         return lastName;
     }
@@ -54,12 +71,22 @@ public class Employee {
         this.lastName = lastName;
     }
 
-    @Column(name = "email_address", nullable = false)
+    @Column(name = "email_address", nullable = false, length = 128)
     public String getEmailId() {
         return emailId;
     }
 
     public void setEmailId(String emailId) {
         this.emailId = emailId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [\n"
+                + "id=" + getId() + ",\n"
+                + "first_name=" + getFirstName() + ",\n"
+                + "last_name=" + getLastName() + ",\n"
+                + "emailId=" + getEmailId() + ",\n"
+                + "]";
     }
 }
